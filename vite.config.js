77feb8ec6c6fb7af55bff98a6eaf8b160867114e
PNurmanM <nurman767659@gmail.com>
2024-11-27 16:24:58 -0600

    gpa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/   "homepage": "https://PNurmanM.github.io/stopWatchApp",
+// https://vite.dev/config/   "homepage": "https://PNurmanM.github.io/TaskTimer",
 export default defineConfig({
-  base: '/stopWatchApp/',
+  base: '/TaskTimer/',
   plugins: [react()],
 })
